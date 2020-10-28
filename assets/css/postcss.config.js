89c130a769ea05a68bf136f261b1c170c54d15b9
rolojf <rolo@tutamail.com>
2020-10-27 20:02:32 -0600

    intentando llamar a dependenfia de forma relativa

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
    plugins: [
       require("postcss-nested"),
       require("postcss-custom-properties"),
-      require("tailwindcss")("assets/css/tailwind.config.js"),
+      require("tailwindcss")("./tailwind.config.js"),
       ...(process.env.HUGO_ENVIROMENT === "production" ? [require("autoprefixer")] : []),
    ],
 };
